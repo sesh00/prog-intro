eb1ecbd0ac4027c65b63ede3b92f5f669d1bf46a
sinne0r <ernestrsage@gmail.com>
2023-06-29 02:53:32 +0300

    Homework 2. Sum of numbers

--- src/Sum.java
+++ src/Sum.java
@@ -1,3 +1,19 @@
 public class Sum {
+    public static void main(String[] args){
+        int sum = 0;
 
+        for (String arg : args) {
+            int i = 0;
+            while (i < arg.length()) {
+                int j;
+                for (j = i; j < arg.length(); j++) {
+                    if (!Character.isDigit(arg.charAt(j)) && arg.charAt(j) != '-') {break;}
+                }
+
+                if (j > i) sum += Integer.parseInt(arg.substring(i, j));
+                i = j + 1;
+            }
+        }
+        System.out.println(sum);
+    }
 }
